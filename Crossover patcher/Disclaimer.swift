bffcb2b6ec9485f473debde57595de12bacad409
Italo Mandara <italomandara@gmail.com>
2023-04-04 15:22:12 +0100

    fix typo in disclaimer

--- Crossover patcher/Disclaimer.swift
+++ Crossover patcher/Disclaimer.swift
@@ -12,7 +12,7 @@ struct Disclaimer: View {
         Text("Please note:")
             .font(.title2)
             .multilineTextAlignment(.center)
-        Text("This is an unofficial patcher for Crossover and codeweavers has nothing to do with this app, and although has tested, this software may render the app unusable or unstable, USE AT YOUR OWN RISK, this also will void any official support from Codeweavers if you have any issues after your app has been patched you can download a new copy of Crossover.")
+        Text("This is an unofficial patcher for Crossover and codeweavers has nothing to do with this app, and although has been tested, this software may render the app unusable or unstable, USE AT YOUR OWN RISK, this also will void any official support from Codeweavers if you have any issues after your app has been patched you can download a new copy of Crossover.")
             .multilineTextAlignment(.center)
             .padding(.top, 1.0)
         Text("for more info: https://www.codeweavers.com/support/forums/general/?t=27;msg=257865")
