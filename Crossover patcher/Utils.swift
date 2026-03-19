f50c02a952f7c995364ed01562bc4c8e348467a4
Italo Mandara <italomandara@gmail.com>
2026-03-19 22:05:55 +0100

    guard DXMTConfigvalues and avoid adding an empty env var

--- Crossover patcher/Utils.swift
+++ Crossover patcher/Utils.swift
@@ -888,7 +888,7 @@ private func addGlobals(url: URL, opts: Opts) {
         console.log("add preferredMaxFrameRate env")
         DXMTConfigvalues += "d3d11.preferredMaxFrameRate=\(String(Int(opts.globalEnvs.preferredMaxFrameRate)));"
     }
-    if(opts.globalEnvs.metalSpatialUpscaleFactor > 0 || opts.globalEnvs.preferredMaxFrameRate > 29.0) {
+    if(!DXMTConfigvalues.isEmpty && (opts.globalEnvs.metalSpatialUpscaleFactor > 0 || opts.globalEnvs.preferredMaxFrameRate > 29.0)) {
         envs += [Env(key: "DXMT_CONFIG", value: DXMTConfigvalues)]
     }
     if(opts.globalEnvs.mtlHudEnabled == true) {
